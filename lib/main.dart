0078a636535b34ce2996ed1cb656b2962daa1a9b
habeebu <habeebrh321@gmail.com>
2023-04-08 12:06:05 +0530

    firebase integrated

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:chat_app/firebase_options.dart';
+import 'package:chat_app/view/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
